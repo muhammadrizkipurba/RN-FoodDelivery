b1b048d97e76f7ca1c37e90ab2d30a6bad12992e
Mhd Rizki Purba <5422573-muhammadrizkipurba@users.noreply.gitlab.com>
2021-09-05 17:25:36 +0800

    adding splash screen

--- App.js
+++ App.js
@@ -1,14 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import 'react-native-gesture-handler'
 import { createStackNavigator } from '@react-navigation/stack'
 import { NavigationContainer } from '@react-navigation/native'
 import CustomDrawer from './navigation/CustomDrawer/CustomDrawer'
 import { Provider } from 'react-redux'
 import store from './redux/globalStore'
+import RNBootSplash from 'react-native-bootsplash';
+
 
 const Stack = createStackNavigator();
 
 const App = () => {
+
+  useEffect(() => {
+    setTimeout(() => {
+      RNBootSplash.hide();
+    }, 3000);
+  }, []);
+
   return (
     <Provider store={store}>
       <NavigationContainer>
